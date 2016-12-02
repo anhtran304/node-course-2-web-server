c912dcce6435f4af433106573f28714a0499c834
Ken Tran <tranmr@gmail.com>
2016-12-02 18:12:15 +1100

    Adding projects page

--- server.js
+++ server.js
@@ -45,7 +45,9 @@ app.get('/about', (req, res) => {
 });
 
 app.get('/projects', (req, res) => {
-  res.render('projects.hbs')
+  res.render('projects.hbs', {
+    pageTitle: 'Projects'
+  })
 });
 
 app.get('/bad', (req, res) => {
